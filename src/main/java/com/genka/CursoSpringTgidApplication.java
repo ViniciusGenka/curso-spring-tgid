baeac70e5fdf28d51b0aa39742c775d957a55905
Vinicius Genka <vini.genka@hotmail.com>
2024-04-29 14:07:15 -0300

    Projeto criado

---
+++ src/main/java/com/genka/CursoSpringTgidApplication.java
@@ -0,0 +1,13 @@
+package com.genka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoSpringTgidApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CursoSpringTgidApplication.class, args);
+    }
+
+}
