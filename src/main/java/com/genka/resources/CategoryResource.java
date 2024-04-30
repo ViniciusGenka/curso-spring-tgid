888de1e896390395a877bb5df89cd0b35f85f8af
Vinicius Genka <vini.genka@hotmail.com>
2024-04-29 23:30:41 -0300

    criação dos endpoints de adicionar e acessar categoria

--- src/main/java/com/genka/resources/CategoryResource.java
+++ src/main/java/com/genka/resources/CategoryResource.java
@@ -1,15 +1,33 @@
 package com.genka.resources;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.genka.domain.Category;
+import com.genka.dtos.CategoryDTO;
+import com.genka.services.CategoryService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/categories")
 public class CategoryResource {
 
-    @GetMapping()
-    public String getCategories() {
-        return "categories";
+    private final CategoryService categoryService;
+
+    public CategoryResource(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Category> getCategoryById(@PathVariable Integer id) {
+        Category category = categoryService.getCategoryById(id);
+        return ResponseEntity.status(200).body(category);
+    }
+
+    @PostMapping()
+    public ResponseEntity<Category> addCategory(@Valid @RequestBody CategoryDTO categoryDTO) {
+        Category categoryToSave = categoryService.mapFromDTO(categoryDTO);
+        Category savedCategory = categoryService.saveCategory(categoryToSave);
+        return ResponseEntity.status(200).body(savedCategory);
     }
 }
