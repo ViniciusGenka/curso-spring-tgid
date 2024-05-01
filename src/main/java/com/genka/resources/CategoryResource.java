b9f3c9bb6210a892b8a4e85bce5e5ef98fab5e9a
Vinicius Genka <vini.genka@hotmail.com>
2024-05-01 17:05:40 -0300

    substituir o status code 200 pela constante HttpStatus.OK

--- src/main/java/com/genka/resources/CategoryResource.java
+++ src/main/java/com/genka/resources/CategoryResource.java
@@ -3,6 +3,7 @@ package com.genka.resources;
 import com.genka.domain.Category;
 import com.genka.dtos.CategoryNewDTO;
 import com.genka.services.CategoryService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,12 +22,12 @@ public class CategoryResource {
     @GetMapping("/{id}")
     public ResponseEntity<Category> getCategoryById(@PathVariable Integer id) {
         Category category = categoryService.getCategoryById(id);
-        return ResponseEntity.status(200).body(category);
+        return ResponseEntity.status(HttpStatus.OK).body(category);
     }
 
     @PostMapping()
     public ResponseEntity<Category> addCategory(@Valid @RequestBody CategoryNewDTO categoryNewDTO) {
         Category savedCategory = categoryService.saveCategory(categoryService.mapFromDTO(categoryNewDTO));
-        return ResponseEntity.status(200).body(savedCategory);
+        return ResponseEntity.status(HttpStatus.OK).body(savedCategory);
     }
 }
