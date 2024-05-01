a130d33e00d4632a70778d2ad32770c92e142136
Vinicius Genka <vini.genka@hotmail.com>
2024-05-01 17:05:40 -0300

    substituir o status code 200 pela constante HttpStatus.OK

--- src/main/java/com/genka/resources/ProductResource.java
+++ src/main/java/com/genka/resources/ProductResource.java
@@ -3,6 +3,7 @@ package com.genka.resources;
 import com.genka.domain.Product;
 import com.genka.dtos.ProductNewDTO;
 import com.genka.services.ProductService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,12 +21,12 @@ public class ProductResource {
     @GetMapping("/{id}")
     public ResponseEntity<Product> getProductById(@PathVariable Integer id) {
         Product product = productService.getProductById(id);
-        return ResponseEntity.status(200).body(product);
+        return ResponseEntity.status(HttpStatus.OK).body(product);
     }
 
     @PostMapping()
     public ResponseEntity<Product> addProduct(@Valid @RequestBody ProductNewDTO productNewDTO) {
         Product savedProduct = productService.saveProduct(productService.mapFromDTO(productNewDTO));
-        return ResponseEntity.status(200).body(savedProduct);
+        return ResponseEntity.status(HttpStatus.OK).body(savedProduct);
     }
 }
