9798deed8395683128df73092e1795f67ffae152
Vinicius Genka <vini.genka@hotmail.com>
2024-05-08 00:14:10 -0300

    padronizar nome da pfp

--- src/main/java/com/genka/services/CustomerService.java
+++ src/main/java/com/genka/services/CustomerService.java
@@ -9,11 +9,13 @@ import com.genka.resources.exceptions.AuthorizationException;
 import com.genka.resources.exceptions.DataIntegrityException;
 import com.genka.resources.exceptions.EntityNotFoundException;
 import com.genka.security.UserAuthDetails;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.awt.image.BufferedImage;
 import java.net.URI;
 import java.util.Optional;
 
@@ -22,11 +24,16 @@ public class CustomerService {
     private final CustomerRepository customerRepository;
     private final BCryptPasswordEncoder bcrypt;
     private final S3Service s3Service;
+    private final ImageService imageService;
 
-    public CustomerService(CustomerRepository customerRepository, BCryptPasswordEncoder bcrypt, S3Service s3Service) {
+    @Value("${img.prefix.client.profile}")
+    private String prefix;
+
+    public CustomerService(CustomerRepository customerRepository, BCryptPasswordEncoder bcrypt, S3Service s3Service, ImageService imageService) {
         this.customerRepository = customerRepository;
         this.bcrypt = bcrypt;
         this.s3Service = s3Service;
+        this.imageService = imageService;
     }
 
     public Optional<Customer> findCustomerById(Integer id) {
@@ -86,7 +93,10 @@ public class CustomerService {
             throw new AuthorizationException("Acesso negado");
         }
         Customer customer = getCustomerByEmail(authenticatedUser.getUsername());
-        URI pfpURI = s3Service.uploadFile(file);
+        BufferedImage jpgImage = imageService.getJpgImageFromFile(file);
+        jpgImage = imageService.cropSquare(jpgImage);
+        String fileName = prefix + customer.getId() + ".jpg";
+        URI pfpURI = s3Service.uploadFile(imageService.getInputStream(jpgImage, "jpg"), fileName, "image");
         customer.setPfpURL(pfpURI.toString());
         customerRepository.save(customer);
         return pfpURI;
